e3f8299212dcab66aaed2fc369b548c8589e7a83
Stefano Ortona <stefano.ortona@meltwater.com>
2019-05-24 12:53:35 +0100

    Add solution for largest sum of average

--- src/main/java/com/ortona/stefano/leetcode/LargestSumOfAverages.java
+++ src/main/java/com/ortona/stefano/leetcode/LargestSumOfAverages.java
@@ -1,62 +1,32 @@
 package com.ortona.stefano.leetcode;
 
 public class LargestSumOfAverages {
+
   public double largestSumOfAverages(int[] A, int K) {
-    int partAvailable=K-1;
-    double sum=0;
-    while(partAvailable>0){
-      int maxIndex=getMaximum(A,partAvailable);
-      sum+=A[maxIndex];
-      partAvailable-=getPartitionIntroduced(A,maxIndex);
-      A[maxIndex]=0;
+    final double[][] dp = new double[K + 1][A.length];
+    final double[] sum = new double[A.length];
+    sum[0] = A[0];
+    dp[1][0] = A[0];
+    for (int i = 1; i < A.length; i++) {
+      sum[i] = sum[i - 1] + A[i];
+      dp[1][i] = sum[i] / (i + 1);
     }
-    //sum other partitions
-    double curSum=0;
-    int curEl=0;
-    for(int i=0;i<A.length;i++){
-      if(A[i]==0){
-        if(curEl>0){
-          sum+=(curSum/curEl);
+    for (int k = 2; k <= K; k++) {
+      dp[k][0] = sum[0];
+      for (int i = 0; i < A.length; i++) {
+        for (int j = 0; j < i; j++) {
+          final double curSum = dp[k - 1][j] + ((sum[i] - sum[j]) / (i - j));
+          dp[k][i] = Math.max(dp[k][i], curSum);
         }
-        curEl=0;
-        curSum=0;
-      } else {
-        curEl++;
-        curSum+=A[i];
       }
     }
-    if(curEl>0){
-      sum+=(curSum/curEl);
-    }
-    return sum;
+    return dp[K][A.length - 1];
   }
 
-  private int getMaximum(int []A, int partitionAvailable){
-    int max=-1;
-    int maxIndex=-1;
-    for(int i=0;i<A.length;i++){
-      if(A[i]!=0 && A[i]>max && getPartitionIntroduced(A,i)<=partitionAvailable){
-        max=A[i];
-        maxIndex=i;
-      }
-    }
-    return maxIndex;  
-  }
-
-  private int getPartitionIntroduced(int []A, int index){
-    int part=0;
-    if(index>0 && A[index-1]!=0){
-      part++;
-    }
-    if(index<A.length-1 && A[index+1]!=0){
-      part++;
-    }
-    return part;
-  }
-  
-  public static void main(String []args){
-    LargestSumOfAverages lS = new LargestSumOfAverages();
-    System.out.println(lS.largestSumOfAverages(new int[]{5870,2722,6249,2196,8717}, 4));
+  public static void main(String[] args) {
+    final LargestSumOfAverages lS = new LargestSumOfAverages();
+    System.out
+        .println(lS.largestSumOfAverages(new int[] { 2561, 9087, 398, 8137, 7838, 7669, 8731, 2460, 1166, 619 }, 3));
   }
 
 }
